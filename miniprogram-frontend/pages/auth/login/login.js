50f062682ea92b94842afa3e667580d26bfb7ce2
lstra <2243649527@qq.com>
2026-02-13 17:34:12 +0800

    登录页面基础结构

--- miniprogram-frontend/pages/auth/login/login.js
+++ miniprogram-frontend/pages/auth/login/login.js
@@ -1,1 +1,30 @@
-Page({});
+Page({
+  data: {
+    username: '',
+    password: ''
+  },
+
+  onUsernameInput(e) {
+    this.setData({
+      username: e.detail.value
+    });
+  },
+
+  onPasswordInput(e) {
+    this.setData({
+      password: e.detail.value
+    });
+  },
+
+  onLogin() {
+    const { username, password } = this.data;
+    console.log('用户名:', username);
+    console.log('密码:', password);
+  },
+
+  onGoRegister() {
+    wx.navigateTo({
+      url: '/pages/auth/register/register'
+    });
+  }
+});

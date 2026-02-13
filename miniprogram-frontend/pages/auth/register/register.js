099f5dccde71ee5dd3bb366e4e4f8279ac76a84f
lstra <2243649527@qq.com>
2026-02-13 21:36:16 +0800

    register 页面及基础验证

--- miniprogram-frontend/pages/auth/register/register.js
+++ miniprogram-frontend/pages/auth/register/register.js
@@ -1,1 +1,64 @@
-Page({});
+Page({
+  data: {
+    username: '',
+    password: '',
+    confirmPassword: ''
+  },
+
+  onUsernameInput(e) {
+    this.setData({
+      username: e.detail.value
+    });
+  },
+
+  onPasswordInput(e) {
+    this.setData({
+      password: e.detail.value
+    });
+  },
+
+  onConfirmPasswordInput(e) {
+    this.setData({
+      confirmPassword: e.detail.value
+    });
+  },
+
+  onRegister() {
+    const { username, password, confirmPassword } = this.data;
+
+    if (!username) {
+      wx.showToast({
+        title: '用户名不能为空',
+        icon: 'none'
+      });
+      return;
+    }
+
+    if (!password) {
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none'
+      });
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      wx.showToast({
+        title: '两次密码不一致',
+        icon: 'none'
+      });
+      return;
+    }
+
+    console.log('注册信息:', {
+      username,
+      password
+    });
+  },
+
+  onBackLogin() {
+    wx.navigateTo({
+      url: '/pages/auth/login/login'
+    });
+  }
+});
